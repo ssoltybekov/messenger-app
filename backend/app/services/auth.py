233421257b93969be1ff552e3a6e5a58a3a89557
Sanzhar <ssoltybekov@gmail.com>
2026-03-14 14:21:32 +0500

    create tokens funcs

--- backend/app/services/auth.py
+++ backend/app/services/auth.py
@@ -1,4 +1,11 @@
 import bcrypt
+from jose import jwt
+from datetime import datetime, timezone, timedelta
+
+JWT_SECRET = "your-super-secret-key" # В Go это был бы []byte("secret")
+JWT_ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+REFRESH_TOKEN_EXPIRE_DAYS=30
 
 def hash_password(password: str) -> str:
     pwd_bytes = password.encode('utf-8')
@@ -11,3 +18,30 @@ def verify_password(password: str, hashed: str) -> bool:
     hashed_bytes = hashed.encode('utf-8')
 
     return bcrypt.checkpw(password_bytes, hashed_bytes)
+
+def create_access_token(user_id: int) -> str:
+    now = datetime.now(timezone.utc)
+    expire = now + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+
+    to_encode = {
+        "user_id": user_id,
+        "exp": expire
+    }
+
+    encoded_jwt = jwt.encode(to_encode, JWT_SECRET, algorithm=JWT_ALGORITHM)
+
+    return encoded_jwt
+
+def create_refresh_token(user_id: int) -> str:
+    now = datetime.now(timezone.utc)
+    expire = now + timedelta(days=REFRESH_TOKEN_EXPIRE_DAYS)
+
+    to_encode = {
+        "user_id": user_id,
+        "exp": expire,
+        "type": "refresh"
+    }
+
+    encoded_jwt = jwt.encode(to_encode, JWT_SECRET, algorithm=JWT_ALGORITHM)
+
+    return encoded_jwt
