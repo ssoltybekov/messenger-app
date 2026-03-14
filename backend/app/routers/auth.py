0be48dffab0833f3a120156967be3aea84dbbb24
Sanzhar <ssoltybekov@gmail.com>
2026-03-14 18:12:36 +0500

    fixed refresh tokens

--- backend/app/routers/auth.py
+++ backend/app/routers/auth.py
@@ -1,10 +1,14 @@
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, status, Body
 from sqlalchemy.orm import Session
 
 from app.schemas.user import UserCreate, UserResponse
 from app.services import auth
 from app.database import get_db
 from app.schemas.token import Token
+from app.models.token import RefreshToken
+import hashlib
+import secrets
+from datetime import datetime, timedelta, timezone
 
 router = APIRouter(
     prefix="/auth",
@@ -23,10 +27,39 @@ def login(login_data: UserCreate, db: Session = Depends(get_db)):
         raise HTTPException(401, "Invalid credentials")
 
     access_token = auth.create_access_token(user.id)
-    refresh_token = auth.create_refresh_token(user.id)
+    # refresh_token = auth.create_refresh_token(user.id)
+
+    refresh_token_raw = secrets.token_urlsafe(32)
+
+    token_hash = hashlib.sha256(refresh_token_raw.encode()).hexdigest()
+    expire_date = datetime.now(timezone.utc) + timedelta(days=30)
+
+    db_refresh_token = RefreshToken(
+        token_hash=token_hash,
+        user_id=user.id,
+        expires_at=expire_date
+    )
+    db.add(db_refresh_token)
+    db.commit()
 
     return {
         "access_token": access_token,
-        "refresh_token": refresh_token, 
+        "refresh_token": refresh_token_raw, 
         "token_type": "bearer"
     }
+
+@router.post("/refresh_tokens", response_model=Token, status_code=200)
+def refresh_access_token(refresh_token: str = Body(..., embed=True), db: Session = Depends(get_db)):
+    new_access_token = auth.refresh_tokens(db, refresh_token=refresh_token)
+
+    return {
+        "access_token": new_access_token,
+        "token_type": "bearer"
+    }
+
+
+@router.post("/logout", status_code=status.HTTP_204_NO_CONTENT)
+def logout(refresh_token: str = Body(..., embed=True), db: Session = Depends(get_db)):
+    auth.logout(db, refresh_token=refresh_token)
+
+    return None
--- backend/app/schemas/token.py
+++ backend/app/schemas/token.py
@@ -1,8 +1,9 @@
 from pydantic import BaseModel
+from typing import Optional
 
 class Token(BaseModel):
     access_token: str
-    refresh_token: str
+    refresh_token: Optional[str] = None
     token_type: str = "bearer"
 
 class TokenPayload(BaseModel):
